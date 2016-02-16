853ef192d060b52095f937fadd8359ec83e26d34
Deepak Kumar <dkumar431@gmail.com>
2016-02-16 06:07:00 +0530

    after restoring the database.yml file

--- app/mailers/example_mailer.rb
+++ app/mailers/example_mailer.rb
@@ -2,9 +2,7 @@ class ExampleMailer < ApplicationMailer
   default from: "dkumar431@gmail.com"
 
   def sample_email(manager,agent)
-    @user = manager
-    @agent = agent
-    mail(to: @user.email, subject: 'Agent Request - Pending Approval')
+    mail(to: manager.email, subject: 'Agent Request - Pending Approval')
   end
 
 end
