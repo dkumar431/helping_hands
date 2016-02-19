beb52d77c97dd73084fad5b584f5747fcbcbf288
Deepak Kumar <dkumar431@gmail.com>
2016-02-20 00:18:38 +0530

    cleaned up dashboard page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,17 +4,20 @@ module ApplicationHelper
   end
 
   def show_errors(object,field_name)
-
     if object.errors.any?
       if !object.errors.messages[field_name].blank?
         object.errors.messages[field_name].join(", ")
       end
     end
-
   end
 
   def header_link
     user_signed_in? ? root_path : new_user_session_path
+  end
+
+
+  def get_role
+    current_user.role.role_name
   end
 
 end
